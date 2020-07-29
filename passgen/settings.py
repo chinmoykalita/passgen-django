6fb6243fd10260a4380636cdf8538f7135edd4bb
chinmoykalita <chinmoykalita210@gmail.com>
2020-07-29 05:12:00 +0000

    changes in static files configurations

--- passgen/settings.py
+++ passgen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8c@3sew5pwbitplzc6u7)!5p-tkka#(1n$!r6!q&3&$l8b8y5m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chinmoykalita.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/chinmoykalita/passgen-django/static'
+
