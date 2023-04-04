1e188bbf535c271103cef53a8d75dca3a61af29a
Chinmoy Kalita <chinmoykalita210@gmail.com>
2023-04-05 01:36:30 +0530

    multiple occuerae

--- passgen1/utils.py
+++ passgen1/utils.py
@@ -6,3 +6,9 @@ def make_hash_pass(inp: str, type: int):
 def hash_format_func(input):
     print(input)
     return input ** 3
+
+def string_to_hash(input):
+    conv = hash_format_func(input)
+    print(conv)
+    ad = conv * 3 - 4
+    return ad
