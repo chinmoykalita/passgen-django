122a0d6ee1c74182cfc51b9928a1cdfb861fb73e
Chinmoy Kalita <chinmoykalita210@gmail.com>
2023-04-10 23:17:55 +0530

    compilation

--- passgen1/utils.py
+++ passgen1/utils.py
@@ -13,6 +13,30 @@ def string_to_hash(input):
     ad = conv * 3 - 4
     return ad
 
+def string_to_hash(input):
+    conv = hash_format_func(input)
+    print(conv)
+    ad = conv * 3 - 4
+    return ad
+
+def string_to_hash(input):
+    conv = hash_format_func(input)
+    print(conv)
+    ad = conv * 3 - 4
+    return ad
+
+def string_to_hash(input):
+    conv = hash_format_func(input)
+    print(conv)
+    ad = conv * 3 - 4
+    return ad
+
+def string_to_hash(input):
+    conv = hash_format_func(input)
+    print(conv)
+    ad = conv * 3 - 4
+    return ad
+
 def function_to_build(request):
     pass
 
