0d3b4d193a63bdb894f59c8e1b10daf075f33cb7
Chinmoy Kalita <chinmoykalita210@gmail.com>
2023-04-17 23:58:36 +0530

    changes to new function

--- passgen1/scripts/passgen.py
+++ passgen1/scripts/passgen.py
@@ -2,3 +2,10 @@ import utils
 
 def get_pivot_out(onsi):
     pass
+
+def edit_in_new_file(request):
+    # this function will be used to identify differrnt types of things in a string 
+    return {
+        "person": "onasodf203f",
+        "new": True
+    }
